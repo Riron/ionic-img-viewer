ce6d14adcc6c88be3a62201cdcb02a321bb961be
Orion <orioncharlier@gmail.com>
2016-11-28 22:14:40 +0100

    Missing export

--- ionic-img-viewer.ts
+++ ionic-img-viewer.ts
@@ -1,2 +1,3 @@
 export { IonicImageViewerModule } from './src/module';
 export { ImageViewerComponent } from './src/image-viewer.component';
+export { ImageViewerDirective } from './src/image-viewer.directive';
