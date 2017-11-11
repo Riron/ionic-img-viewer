4ae796e13dcd00ed581e5fbe9d643f45b67b2d26
Orion <orioncharlier@gmail.com>
2017-11-11 14:03:27 +0100

    Lettable operators

--- src/image-viewer-impl.ts
+++ src/image-viewer-impl.ts
@@ -1,5 +1,4 @@
-import 'rxjs/add/operator/delay';
-import 'rxjs/add/observable/zip';
+import { delay, zip } from 'rxjs/operators';
 
 import { App, Config, NavOptions, ViewController } from 'ionic-angular';
 import { Observable } from 'rxjs/Observable';
@@ -9,50 +8,60 @@ import { ImageViewerEnter, ImageViewerLeave } from './image-viewer-transitions';
 import { ImageViewerComponent } from './image-viewer.component';
 
 export class ImageViewerImpl extends ViewController {
+  constructor(
+    private app: App,
+    component: ImageViewerComponent,
+    opts: ImageViewerOptions = {},
+    config: Config
+  ) {
+    super(component, opts);
 
-	constructor(private app: App, component: ImageViewerComponent, opts: ImageViewerOptions = {}, config: Config) {
-		super(component, opts);
+    config.setTransition('image-viewer-enter', ImageViewerEnter);
+    config.setTransition('image-viewer-leave', ImageViewerLeave);
 
-		config.setTransition('image-viewer-enter', ImageViewerEnter);
-		config.setTransition('image-viewer-leave', ImageViewerLeave);
+    this.didLeave.subscribe(
+      () => opts.onCloseCallback && opts.onCloseCallback()
+    );
 
-		this.didLeave.subscribe(() => opts.onCloseCallback && opts.onCloseCallback());
+    this.willEnter.subscribe(() =>
+      this.handleHighResImageLoad(opts.fullResImage)
+    );
+  }
 
-		this.willEnter.subscribe(() => this.handleHighResImageLoad(opts.fullResImage));
-	}
+  getTransitionName(direction: string) {
+    return `image-viewer-${direction === 'back' ? 'leave' : 'enter'}`;
+  }
 
-	getTransitionName(direction: string) {
-		return `image-viewer-${direction === 'back' ? 'leave' : 'enter'}`;
-	}
+  present(navOptions: NavOptions = {}) {
+    return this.app.present(this, navOptions);
+  }
 
-	present(navOptions: NavOptions = {}) {
-		return this.app.present(this, navOptions);
-	}
+  private handleHighResImageLoad(fullResImage) {
+    if (!fullResImage) {
+      return;
+    }
 
-	private handleHighResImageLoad(fullResImage){
-		if (!fullResImage) {
-			return;
-		}
+    const image = new Image();
+    image.src = fullResImage;
 
-		const image = new Image();
-		image.src = fullResImage;
+    if (!image.complete) {
+      const onLoadObservable = Observable.create(obs => {
+        image.onload = () => {
+          obs.next(image);
+          obs.complete();
+        };
+      });
 
-		if (!image.complete) {
-			const onLoadObservable = Observable.create(obs => {
-				image.onload = () => {
-					obs.next(image);
-					obs.complete();
-				}
-			});
-
-			// We want the animation to finish before replacing the pic
-			// as the calculation has been done with the smaller image
-			// AND, to avoid a flash if it loads "too quickly", wait at least 300ms after didEnter
-			Observable.zip(onLoadObservable, this.didEnter.delay(300))
-				.subscribe(() => this.instance.updateImageSrcWithTransition(fullResImage));
-
-		} else {
-			this.instance.updateImageSrc(fullResImage)
-		}
-	}
+      // We want the animation to finish before replacing the pic
+      // as the calculation has been done with the smaller image
+      // AND, to avoid a flash if it loads "too quickly", wait at least 300ms after didEnter
+      onLoadObservable
+        .pipe(zip(this.didEnter.pipe(delay(300))))
+        .subscribe(() =>
+          this.instance.updateImageSrcWithTransition(fullResImage)
+        );
+    } else {
+      this.instance.updateImageSrc(fullResImage);
+    }
+  }
 }
